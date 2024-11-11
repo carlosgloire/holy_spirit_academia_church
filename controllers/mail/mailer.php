43802e34ad7afcf9a7f67c55e6583cf2a0a282d7
carlosgloire <ndayisabagloire96@gmail.com>
2024-11-12 01:23:11 +0200

    Holy spirit academia church

--- controllers/mail/mailer.php
+++ controllers/mail/mailer.php
@@ -3,7 +3,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require_once('../mail/vendor/autoload.php');
+require __DIR__ . "/vendor/autoload.php";
 
 $mail = new PHPMailer(true);
 //$mail->SMTPDebug = SMTP::DEBUG_SERVER; // Enable verbose debug output
