907b59a818e39eca73c6a27e0b854756b760faf2
carlosgloire <ndayisabagloire96@gmail.com>
2024-11-11 22:43:51 +0200

    Holy spirit academia church

--- database/db.php
+++ database/db.php
@@ -1,3 +1,3 @@
 <?php
-$db = new PDO("mysql:host=82.112.1;dbname=holy_spirit_academia_church", "mysql", "hsac@_230",
- [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+$db = new PDO("mysql:host=82.112.225.123;port=3306;dbname=holy_spirit_academia_church", "mysql", "hsac@_230", 
+[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
