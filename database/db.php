f406fac26580096a12e05f06541233c141be1121
carlosgloire <ndayisabagloire96@gmail.com>
2024-11-11 23:45:46 +0200

    Holy spirit academia church

--- database/db.php
+++ database/db.php
@@ -1,3 +1,9 @@
 <?php
-$db = new PDO("mysql:host=82.112.225.123;port=3306;dbname=holy_spirit_academia_church", "mysql", "hsac@_230", 
-[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+try {
+    $db = new PDO("mysql:host=82.112.225.123;port=3306;dbname=holy_spirit_academia_church", "mysql", "hsac@_230", 
+    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+} catch (PDOException $e) {
+    error_log("Database connection error: " . $e->getMessage(), 3, "/app/error_log.txt");
+    echo 'Connection failed: Check error log for details';
+}
+?>
