83e98c748f9c21b14b923c90e8ec18eebffb6778
carlosgloire <ndayisabagloire96@gmail.com>
2024-11-12 00:21:27 +0200

    Holy spirit academia church

--- controllers/mail/database.php
+++ controllers/mail/database.php
@@ -1,15 +1,17 @@
 <?php
 
-$host = "localhost";
-$dbname = "hsac";
-$username = "root";
-$password = "";
+$host = "holy_spirit_academia_church_hsac"; // Internal Host from your credentials
+$dbname = "holy_spirit_academia_church";    // Database Name
+$username = "mysql";                        // User
+$password = "hsac@_230";                    // Password
 
-$mysqli = new mysqli(hostname: $host,
-                     username: $username,
-                     password: $password,
-                    database: $dbname);
-                     
+$mysqli = new mysqli(
+    hostname: $host,
+    username: $username,
+    password: $password,
+    database: $dbname
+);
+
 if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
 }
