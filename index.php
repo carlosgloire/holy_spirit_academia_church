9ced5ad097437b70783e4e94fdbe76b4da5b1a81
carlosgloire <ndayisabagloire96@gmail.com>
2024-11-15 17:26:21 +0200

    Holy spirit academia church

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Découvrez Holy Spirit Academia, votre guide spirituel avec des sermons, des ressources et un engagement communautaire.">
+    <meta name="keywords" content="Holy Spirit Academia, sermons, ressources d'église, e-books, croissance spirituelle">
+
     <title>Accueil</title>
 
     <!--css-->
